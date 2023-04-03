942b6fdcad3177e9a30aff626294eafdfb6a2c65
saurabh sharma <saurabhcoded@gmail.com>
2023-04-04 00:36:14 +0530

    🐛Bug Fix

--- client/src/layout/components/BlogCard.jsx
+++ client/src/layout/components/BlogCard.jsx
@@ -7,7 +7,7 @@ const BlogCard = () => {
             <Image width={300} height={200} src="/images/blogs/blog1.jpg" className="card-img-top rounded" alt="blog" style={{ objectFit: "cover" }} />
             <div className="card-body p-0 mt-2">
                 <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card&apos;s content.</p>
                 <a href="#" className="btn btn-primary">Go somewhere</a>
             </div>
         </div>
